d2f3fa45a9e46c1682979522d101645826d4de70
theminacious <zacksunrise32@gmail.com>
2024-12-19 01:35:52 +0530

    `Added login functionality to user controller and routes`

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -16,7 +16,7 @@ module.exports.registerUser = async(req,res,next)=>{
        const hashedPassword = await userModel.hashPassword(password);
 
        const user = await userService.createuser({
-        firstname:fullname.firstname,
+        firstname:fullname.firstname, 
         lastname:fullname.lastname,
         email,
         password:hashedPassword});
@@ -28,4 +28,26 @@ module.exports.registerUser = async(req,res,next)=>{
         next(error);
     }
 
+}
+
+module.exports.loginUser = async(req,res,next)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
+    try{
+        const {email,password} = req.body;
+        const user = await userModel.findOne({email}).select("+password");
+        if(!user){
+            return res.status(400).json({message:"Invalid credentials"});
+        }
+        const isPasswordValid = await user.comparePassword(password);
+        if(!isPasswordValid){
+            return res.status(400).json({message:"Invalid password"});
+        }
+        const token = user.generateAuthToken();
+        return res.status(200).json({user,token});
+    }catch(error){
+        next(error);
+    }
 }
--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -15,16 +15,15 @@ router.post(
       .isLength({ min: 6 })
       .withMessage("Password must be at least 6 characters long"),
   ],
-//   (req, res) => {
-//     // Validate the request
-//     const errors = validationResult(req);
-//     if (!errors.isEmpty()) {
-//       return res.status(400).json({ errors: errors.array() });
-//     }
-
-//     // Call the controller function to handle business logic
     userController.registerUser
-//   }
+
 );
+
+router.post("/login",[
+  body("email").isEmail().withMessage("Please enter a valid email"),
+  body("password")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters long"),
+], userController.loginUser);
 
 module.exports = router;
