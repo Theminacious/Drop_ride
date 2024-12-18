4de45855c5e241cf38f45ca35b0e614393b08841
theminacious <zacksunrise32@gmail.com>
2024-12-19 01:06:40 +0530

    `Update MongoDB connection in db.js to include connection options and improved error handling.`

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -2,11 +2,15 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.error(`Error connecting to MongoDB: ${error.message}`);
-    // process.exit(1); // Exit process with failure
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB is connected");
+  } catch (err) {
+    
+    console.error(`Error connecting to MongoDB: ${err.message}`);
+    process.exit(1);
   }
 };
 
