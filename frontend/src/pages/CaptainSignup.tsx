1911eb667066b0a1c73c58c5f6055a719e0689aa
theminacious <zacksunrise32@gmail.com>
2024-12-21 02:12:47 +0530

    Setup for frontend setting tailwind and created pages

---
+++ frontend/src/pages/CaptainSignup.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const CaptainSignup = () => {
+  return (
+    <div className="captain-signup-container">
+      <h1>Captain Signup</h1>
+      <form>
+        {/* Add form fields here */}
+      </form>
+    </div>
+  );
+};
+
+export default CaptainSignup;
